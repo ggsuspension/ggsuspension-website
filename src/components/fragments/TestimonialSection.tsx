d2d11de9a5fc53bc5a7d7235ce4857893f69e5f1
timotiusdwiyuliantomalau <timotiusdym17@gmail.com>
2025-02-24 15:04:53 +0700

    Ganti foto testimoni

--- src/components/fragments/TestimonialSection.tsx
+++ src/components/fragments/TestimonialSection.tsx
@@ -1,21 +1,38 @@
 // components/TestimonialsSection.tsx
-import { MessageCircle, Star } from 'lucide-react';
+import { Star } from "lucide-react";
 
 export const TestimonialsSection = () => {
-  const arrayTesti=[{nama:"Mitsu Simanjutak",review:"Pelayanannya bgs dan rapih.. Bestlah pokok e"}, {nama:"Irfan Ikhwanda",review:"Recommendedd bgt service di sinii"}, {nama:"Rully Trisatyo",review:"Kemaren bru aja downsize di sini, mtor jdi mantul"}]
+  const arrayTesti = [
+    {
+      nama: "Mitsu Simanjutak",
+      review: "Pelayanannya bgs dan rapih.. Bestlah pokok e",
+    },
+    { nama: "Irfan Ikhwanda", review: "Recommendedd bgt service di sinii" },
+    {
+      nama: "Rully Trisatyo",
+      review: "Kemaren bru aja downsize di sini, mtor jdi mantul",
+    },
+  ];
   return (
     <section id="testimoni" className="pt-[5em] pb-[10em] bg-gray-50">
       <div className="px-5">
         <span className="flex flex-col items-center">
-          <h1 className="text-4xl tablet:text-6xl font-bold text-center text-red-600">APA ?</h1>
-          <h2 className="text-xl tablet:text-3xl font-bold text-center mb-5">KATA MEREKA</h2>
+          <h1 className="text-4xl tablet:text-6xl font-bold text-center text-red-600">
+            APA ?
+          </h1>
+          <h2 className="text-xl tablet:text-3xl font-bold text-center mb-5">
+            KATA MEREKA
+          </h2>
         </span>
         <div className="grid md:grid-cols-3 gap-8">
-          {arrayTesti.map((item,i) => (
+          {arrayTesti.map((item, i) => (
             <div key={i} className="bg-white p-6 rounded-lg shadow-md">
               <div className="flex items-center mb-4">
-                <div className="bg-orange-100 w-12 h-12 rounded-full flex items-center justify-center mr-4">
-                  <MessageCircle className="w-6 h-6 text-orange-600" />
+                <div className="bg-orange-100 overflow-hidden w-12 h-12 rounded-full flex items-center justify-center mr-4">
+                  <img
+                    src="https://thispersondoesnotexist.com/"
+                    className="w-full h-full text-orange-600"
+                  />
                 </div>
                 <div>
                   <h4 className="font-semibold">{item.nama}</h4>
@@ -26,9 +43,7 @@ export const TestimonialsSection = () => {
                   </div>
                 </div>
               </div>
-              <p className="text-gray-600">
-                {item.review}
-              </p>
+              <p className="text-gray-600">{item.review}</p>
             </div>
           ))}
         </div>
