b746f2c73c0522bd6b5e54b2e28336bc1ce94696
nazilanrrhmn <nazilanurr99@gmail.com>
2025-04-27 11:36:04 +0700

    first commit2

--- src/components/fragments/TabelAntrianHarian.tsx
+++ src/components/fragments/TabelAntrianHarian.tsx
@@ -10,7 +10,7 @@ const TabelAntrianHarian: React.FC<TabelAntrianHarianProps> = ({ data }) => {
       <p className="text-center text-gray-500 mt-4">Tidak ada data antrian.</p>
     );
   }
-  console.log(data)
+  console.log(data);
 
   return (
     <div className="mt-6">
@@ -60,7 +60,6 @@ const TabelAntrianHarian: React.FC<TabelAntrianHarianProps> = ({ data }) => {
                       </td>
                       <td className="py-2 px-4 border-b">{displayStatus}</td>
                     </tr>
-<<<<<<< HEAD
                   );
                 })}
               </tbody>
@@ -68,160 +67,6 @@ const TabelAntrianHarian: React.FC<TabelAntrianHarianProps> = ({ data }) => {
           </div>
         </div>
       ))}
-=======
-                  </thead>
-                  <tbody className="block md:table-row-group">
-                    {row.data
-                      .sort((a: any, b: any) => a.waktu - b.waktu)
-                      .map((item: any, i: number) => (
-                        <tr
-                          key={i}
-                          className="bg-white border-b block md:table-row text-center relative"
-                        >
-                          <td
-                            className="border p-2 text-center block md:table-cell"
-                            data-label="Nomor"
-                          >
-                            {i + 1}
-                          </td>
-                          <td
-                            className="border p-2 block md:table-cell"
-                            data-label="Nama"
-                          >
-                            {item.nama.toUpperCase()}
-                          </td>
-                          <td
-                            className="border p-2 block md:table-cell"
-                            data-label="Nama"
-                          >
-                            {item.plat.toUpperCase()}
-                          </td>
-                          <td
-                            className="border p-2 block md:table-cell"
-                            data-label="No WA"
-                          >
-                            {item.noWA.toUpperCase()}
-                          </td>
-                          <td
-                            className="border p-2 block md:table-cell"
-                            data-label="Layanan"
-                          >
-                            {item.layanan}
-                          </td>
-                          <td
-                            className="border p-2 block md:table-cell"
-                            data-label="Bagian Motor"
-                          >
-                            {item.jenisMotor.toUpperCase()}
-                          </td>
-                          <td
-                            className="border p-2 block md:table-cell"
-                            data-label="Bagian Motor"
-                          >
-                            {item.bagianMotor.toUpperCase()}
-                          </td>
-                          <td
-                            className="border p-2 block md:table-cell"
-                            data-label="Bagian Motor"
-                          >
-                            {item.bagianMotor2
-                              ? item.bagianMotor2.toUpperCase()
-                              : "-"}
-                          </td>
-                          <td
-                            className="border p-2 block md:table-cell"
-                            data-label="Motor"
-                          >
-                            {item.motor.toUpperCase()}
-                          </td>
-                          <td
-                            className="border p-2 block md:table-cell"
-                            data-label="Harga"
-                          >
-                            {item.seal == "false"
-                              ? "-"
-                              : item.seal.toUpperCase()}
-                          </td>
-                          <td
-                            className="border p-2 block md:table-cell"
-                            data-label="Harga"
-                          >
-                            {item.hargaLayanan}
-                          </td>
-                          <td
-                            className="border p-2 block md:table-cell"
-                            data-label="Harga"
-                          >
-                            {item.hargaSeal}
-                          </td>
-                          <td
-                            className="border p-2 block md:table-cell w-fit"
-                            data-label="Harga"
-                          >
-                            {item.totalHarga
-                              .toString()
-                              .replace(/\B(?=(\d{3})+(?!\d))/g, ".")}
-                          </td>
-                          <td
-                            className="border p-2 block md:table-cell w-fit"
-                            data-label="Harga"
-                          >
-                            {item.info.toUpperCase()}
-                          </td>
-                          <td
-                            className="border p-2 block md:table-cell w-fit"
-                            data-label="Harga"
-                          >
-                            {item.sumber_info
-                              ? item.sumber_info.toUpperCase()
-                              : ""}
-                          </td>
-                          <td
-                            className="border p-2 block md:table-cell"
-                            data-label="Status"
-                          >
-                            {row.status == true ? (
-                              <span className="flex items-center font-semibold text-green-600 gap-1 justify-center">
-                                <span className="bg-green-500 w-5 h-5 rounded-full" />
-                                FINISH
-                              </span>
-                            ) : (
-                              <span className="flex items-center text-yellow-500 gap-1 font-semibold justify-center">
-                                <span className="bg-yellow-500 w-5 h-5 rounded-full" />
-                                PROGRESS
-                              </span>
-                            )}
-                          </td>
-                          {!row.status && (
-                            <div className="flex items-center  absolute top-1/2 -translate-y-1/2 ml-2 gap-2">
-                              <span
-                                onClick={() => handleFinishNow(item)}
-                                className="text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-1 text-center cursor-pointer"
-                              >
-                                Finish
-                              </span>
-                              <span
-                                onClick={() => handleCancel(item.id)}
-                                className="text-white bg-red-500 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-1 text-center cursor-pointer"
-                              >
-                                Cancel
-                              </span>
-                            </div>
-                          )}
-                        </tr>
-                      ))}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          ))
-        ) : (
-          <p className="text-xl text-center text-yellow-500 font-bold">
-            loading....
-          </p>
-        )}
-      </div>
->>>>>>> 779958a45086e399c5e2a32852ec5007782f8021
     </div>
   );
 };
